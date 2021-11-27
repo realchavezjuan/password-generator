f4e8ebfd23771fde31ce6f78c83e37b41356805a
Juan Chavez <juan@Juans-Air-12.home>
2021-11-26 23:45:32 -0800

    finished project by fixing prompts and conditional statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,17 @@
 // Assignment code here
-var generatePassword = function(){
-  //gets how many characters in password INTEGER
-  var passLength = window.prompt("How many characters do you want in your in password? (8min-8max)");
-  passLength = parseInt(passLength);
+var selectAPrompt = function(passLength){
   var pass = "";
 
-  if(passLength>7 && passLength<129){
-  
-    //asks if user wants uppercase letters BOOL
-    var upperCaseConfirm = window.confirm("Do you want upper case characters in your password?");
-    //asks if user wants numbers BOOL
-    var numbersConfirm = window.confirm("Do you want numbers in your password?");
-    //asks if user wants special characters BOOL
-    var specialCharConfirm = window.confirm("Do you want special characters in your password?");
+  //asks if user wants lowercase letters 
+  var lowerCaseConfirm = window.confirm("Do you want lower case characters in your password?");
+  //asks if user wants uppercase letters BOOL
+  var upperCaseConfirm = window.confirm("Do you want upper case characters in your password?");
+  //asks if user wants numbers BOOL
+  var numbersConfirm = window.confirm("Do you want numbers in your password?");
+  //asks if user wants special characters BOOL
+  var specialCharConfirm = window.confirm("Do you want special characters in your password?");
 
+  if (lowerCaseConfirm || upperCaseConfirm || numbersConfirm || specialCharConfirm){
     //arrays for password generator
     var lowerCaseAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upperCaseAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -21,27 +19,25 @@ var generatePassword = function(){
     var specialChars = ['!','@','#','$','%','^','&','*','(',')'];
 
     //creating the list for the for loop based on the users choice selection
-    var list = lowerCaseAlpha;
+    var list = [];
 
+    if(lowerCaseConfirm){
+      list = list.concat(lowerCaseAlpha);
+    }
     if (upperCaseConfirm){
       list = list.concat(upperCaseAlpha);
-      console.log(list);
     }
     if (numbersConfirm) {
       list = list.concat(integers);
-      console.log(list);
     }
     if(specialCharConfirm) {
       list =list.concat(specialChars);
-      console.log(list);
     }
 
     for(var i=0; i<passLength; i++){
-      
+      var random = Math.random();
       //gets random character from array
-      numForArray = Math.round(Math.random() * list.length);
-      console.log(list[numForArray]);
-
+      var numForArray = Math.round(random * (list.length - 1));
       //adds new character to string one at a time
       pass += list[numForArray];
 
@@ -49,7 +45,23 @@ var generatePassword = function(){
     console.log(pass);
   }
   else{
-    window.alert("type a number greater than 8 and less than 128");
+    window.alert("You must pick atleast one option.");
+    pass = selectAPrompt(passLength);
+  }
+  return pass;
+}
+
+var generatePassword = function(){
+  //gets how many characters in password INTEGER
+  var passLength = window.prompt("How many characters do you want in your in password? (8min-8max)");
+  passLength = parseInt(passLength);
+  var pass = "";
+
+  if(passLength>7 && passLength<129){
+    pass = selectAPrompt(passLength);
+  }
+  else{
+    window.alert("Type a number greater than 8 and less than 128");
   }
   return pass;
 }  
@@ -63,7 +75,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-
 }
 
 
